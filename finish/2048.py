8b035b35d39e7124844036927dd1c4a14cc4ea09
josebraz <jbraz@nelogica.com.br>
2022-05-24 20:10:52 -0300

    Otimizações no 2048

--- finish/2048.py
+++ finish/2048.py
@@ -3,10 +3,10 @@ from tkinter import *
 from random import randint
 from copy import deepcopy
 
-size = 400
-margin = 6
 items = 4
-item_size = size / items
+item_size = 100
+size = item_size * items
+margin = item_size/20
 
 EMPTY_VALUE = -1
 LEFT  = 0
@@ -20,12 +20,8 @@ text_color_low = "#70675e"
 text_color_high = "#fff5e9"
 values_colors = ["#eee4db", "#ece0c8", "#f3b177", "#f49662", "#f57c5f", "#f65f3c", "#ebcf72", "#edcc63", "#ebc850", "#ecc53e", "#efc32e"]
 
-values = [
-    [EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE],
-    [EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE],
-    [EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE],
-    [EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE]
-]
+values = [[EMPTY_VALUE for _ in range(items)] for _ in range(items)]
+old_values = [[0 for _ in range(items)] for _ in range(items)]
 
 # desenha todos os quadados do jogo
 def draw_game():
@@ -35,7 +31,10 @@ def draw_game():
 
 # desenha um quadrado
 def draw_square(x, y, value = EMPTY_VALUE):
-    global w
+    global w, old_values
+    if (old_values[y][x] == value): 
+        # otimização: não precisa redesenhar porque o valor não mudou
+        return
     if value == -1:
         color = empty_color
     else:
@@ -58,7 +57,7 @@ def draw_square(x, y, value = EMPTY_VALUE):
             y * item_size + item_size/2 + margin,
             text=str(value),
             fill=text_color,
-            font=('Helvetica','30','bold'),
+            font=('Helvetica', int(item_size/3.5), 'bold'),
             justify="center")
 
 # verifica se tem espaço em branco no grid do jogo
@@ -73,21 +72,35 @@ def generate_new_value():
     if not has_space_for_new_value():
         # não tem lugar disponível para gerar um novo valor
         return
-    new_x = randint(0, items-1)
-    new_y = randint(0, items-1)
-    if values[new_y][new_x] != EMPTY_VALUE:
-        generate_new_value()
-    else:
-        values[new_y][new_x] = 2
+    while True:
+        new_x = randint(0, items-1)
+        new_y = randint(0, items-1)
+        if values[new_y][new_x] == EMPTY_VALUE:
+            values[new_y][new_x] = 2
+            break          
 
 # verifica se o jogo acabou, ou seja, 
 # não tem mais ações possíveis que o jogador pode fazer
 def verify_end_game():
+    # otimização: se tem um espaço disponível, tem jogada possível
+    if has_space_for_new_value():
+        return
     # testa o jogo ainda tem alguma ação possível, se não tem acabou o jogo
     copy = deepcopy(values)
-    if not (left_action(copy) or right_action(copy) or down_action(copy) or up_action(copy)):
-        print("GAME OVER")
-        master.destroy()
+    left_action(copy)
+    if (copy != values):
+        return
+    right_action(copy)
+    if (copy != values):
+        return
+    down_action(copy)
+    if (copy != values):
+        return
+    up_action(copy)
+    if (copy != values):
+        return
+    print("GAME OVER")
+    master.destroy()
 
 # faz as ações de um turno do jogo, chamado quando o usuário toca em uma seta
 def game_step():
@@ -96,108 +109,77 @@ def game_step():
     draw_game()
     verify_end_game()
 
+def filter_empty_value(list):
+    filtered = []
+    for value in list:
+        if value != EMPTY_VALUE:
+            filtered.append(value)
+    return filtered
+
+def get_column(matrix, i):
+    return [row[i] for row in matrix]
+
+def set_column(matrix, i, value):
+    index = 0
+    for row in matrix:
+        row[i] = value[index]
+        index += 1
+
+def combine(list):
+    filtered = filter_empty_value(list)
+    new_list = []
+    combined_index = -1
+    for index, value in enumerate(filtered):
+        if combined_index != index:
+            if index < len(filtered)-1 and filtered[index+1] == value:
+                new_list.append(value * 2)
+                combined_index = index+1
+            else:
+                new_list.append(value)
+    return new_list + [EMPTY_VALUE] * (items - len(new_list))
+
 def left_action(values):
-    any_change = False
     for y in range(items):
-        combine = -1
-        for x in range(items-1, 0, -1):
-            if values[y][x] != EMPTY_VALUE:
-                if values[y][x] == values[y][x-1] and combine != x:
-                    combine = x-1
-                    values[y][x-1] *= 2
-                    values[y][x] = EMPTY_VALUE
-                    any_change = True
-                elif values[y][x-1] == EMPTY_VALUE:
-                    values[y][x-1] = values[y][x]
-                    values[y][x] = EMPTY_VALUE
-                    any_change = True
-            if x < items-1 and values[y][x] == EMPTY_VALUE and values[y][x+1] != EMPTY_VALUE:
-                values[y][x] = values[y][x+1]
-                values[y][x+1] = EMPTY_VALUE
-                any_change = True
-    return any_change
+        new_list = combine(values[y])
+        values[y] = new_list
 
 def right_action(values):
-    any_change = False
     for y in range(items):
-        combine = -1
-        for x in range(items):
-            if x < items-1 and values[y][x] != EMPTY_VALUE:
-                if values[y][x] == values[y][x+1] and combine != x:
-                    combine = x+1
-                    values[y][x+1] *= 2
-                    values[y][x] = EMPTY_VALUE
-                    any_change = True
-                elif values[y][x+1] == EMPTY_VALUE:
-                    values[y][x+1] = values[y][x]
-                    values[y][x] = EMPTY_VALUE
-                    any_change = True
-            if x > 0 and x < items-1 and values[y][x] == EMPTY_VALUE and values[y][x-1] != EMPTY_VALUE:
-                values[y][x] = values[y][x-1]
-                values[y][x-1] = EMPTY_VALUE
-                any_change = True
-    return any_change
+        new_list = list(reversed(combine(reversed(values[y]))))
+        values[y] = new_list
 
 def down_action(values):
-    any_change = False
     for x in range(items):
-        combine = -1
-        for y in range(items):
-            if y < items-1 and values[y][x] != EMPTY_VALUE:
-                if values[y][x] == values[y+1][x] and combine != y:
-                    combine = y+1
-                    values[y+1][x] *= 2
-                    values[y][x] = EMPTY_VALUE
-                    any_change = True
-                elif values[y+1][x] == EMPTY_VALUE:
-                    values[y+1][x] = values[y][x]
-                    values[y][x] = EMPTY_VALUE
-                    any_change = True
-            if y > 0 and y < items-1 and values[y][x] == EMPTY_VALUE and values[y-1][x] != EMPTY_VALUE:
-                values[y][x] = values[y-1][x]
-                values[y-1][x] = EMPTY_VALUE
-                any_change = True
-    return any_change
+        new_list = list(reversed(combine(reversed(get_column(values, x)))))
+        set_column(values, x, new_list)
 
 def up_action(values):
-    any_change = False
     for x in range(items):
-        combine = -1
-        for y in range(items-1, 0, -1):
-            if values[y][x] != EMPTY_VALUE:
-                if values[y][x] == values[y-1][x] and combine != y:
-                    combine = y-1
-                    values[y-1][x] *= 2
-                    values[y][x] = EMPTY_VALUE
-                    any_change = True
-                elif values[y-1][x] == EMPTY_VALUE:
-                    values[y-1][x] = values[y][x]
-                    values[y][x] = EMPTY_VALUE
-                    any_change = True
-            if y < items-1 and values[y][x] == EMPTY_VALUE and values[y+1][x] != EMPTY_VALUE:
-                values[y][x] = values[y+1][x]
-                values[y+1][x] = EMPTY_VALUE
-                any_change = True
-    return any_change
+        new_list = combine(get_column(values, x))
+        set_column(values, x, new_list)
 
 # funções chamadas quando o usuário clica em alguma seta
 def left_key(event):
-    global values
+    global values, old_values
+    old_values = deepcopy(values)
     left_action(values)
     game_step()
 
 def right_key(event):
-    global values
+    global values, old_values
+    old_values = deepcopy(values)
     right_action(values)
     game_step()
 
 def down_key(event):
-    global values
+    global values, old_values
+    old_values = deepcopy(values)
     down_action(values)
     game_step()
 
 def up_key(event):
-    global values
+    global values, old_values
+    old_values = deepcopy(values)
     up_action(values)
     game_step()
 
